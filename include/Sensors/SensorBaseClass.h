8099663c6cccf51ecec2574c3ff923c5fdfac750
andrewli <andy.li3964@gmail.com>
2024-03-15 00:15:54 -0400

    Cleaned up SensorBaseClass, added debugging virtual method

--- include/Sensors/SensorBaseClass.h
+++ include/Sensors/SensorBaseClass.h
@@ -1,13 +1,22 @@
 class SensorBaseClass {
     private: 
-        int _receivePin; // underscore used to differentiate private from public variables
-        int _transmitPin;
+        const int _receivePin; // underscore used to differentiate private from public variables
+        const int _transmitPin;
+        const int _baudRate;
 
     public:
-        SensorBaseClass(int receivePin, int transmitPin);
-        ~SensorBaseClass(); // NO IMPLEMENTATION destructor function, called when object is destroyed 
+        SensorBaseClass(int receivePin, int transmitPin, int baudRate) : 
+        _receivePin(receivePin), _transmitPin(transmitPin), _baudRate(baudRate) {
+        }
 
-        void setReceivePin(int receivePin);
-        void setTransmitPin(int transmitPin);
+        // having a virtual destructor is not necessary, but a good practice for base classes
+        virtual ~SensorBaseClass(); 
+
+        int getReceivePin();
+        int getTransmitPin();
+        int getBaudRate();
+
+        // pure virtual function, must be implemented by derived classes
+        virtual void debugPrintData() = 0;
 };
 
--- src/Sensors/SensorBaseClass.cpp
+++ src/Sensors/SensorBaseClass.cpp
@@ -1,22 +1,26 @@
+#include <Arduino.h>
+#include "../../include/Sensors/SensorBaseClass.h"
+
+
 class SensorBaseClass {
     
     private: 
         int _receivePin = 0; // underscore used to differentiate private from public variables
         int _transmitPin = 0;
-
+        int _baudRate = 0;
 
     public:
 
-        SensorBaseClass(int receivePin, int transmitPin) {
-            _receivePin = receivePin;
-            _transmitPin = transmitPin;
+        int getReceivePin() {
+            return _receivePin;
         }
 
-        void SensorBaseClass::setReceivePin(int receivePin) {
-            _receivePin = receivePin;
+        int getTransmitPin() {
+            return _transmitPin;
         }
 
-        void SensorBaseClass::setTransmitPin(int transmitPin) {
-            _transmitPin = transmitPin;
+        int getBaudRate() {
+            return _baudRate;
         }
+
 };
